45e45c04ca6ac3fe06b092a79277d77e0c12dc90
0712023 <0712023@hanmail.net>
2020-12-19 17:02:31 +0900

    sync

--- src/semiconductor/view/RunningStartView.java
+++ src/semiconductor/view/RunningStartView.java
@@ -10,19 +10,18 @@ public class RunningStartView {
 	public static void main(String[] args) {
 		ProcessController con = ProcessController.getInstance();
 		System.out.println("------------------------");
-//		con.addProcess1(7);
+//		con.addProcess(7);
 //		con.updateClean1(7, 15);
 //		con.updateClean2(7, 17);
-//		con.addProcess1(8);		
+//		con.addProcess(8);		
 //		con.updateRinse1(7, 16);
 //		con.updateClean1(8, 14);
 //		con.updateClean2(8, 30);
 //		for (int i = 1; i<= 6; i++) {
-//			con.addui(i);
 //			con.updateUi(i);
 //		}
-		System.out.println("------------------------");
-		con.getAllProcess1();
+//		System.out.println("------------------------");
+//		con.getAllProcess1();
 //		System.out.println("------------------------");
 //		con.getAllProcess2();
 	}
